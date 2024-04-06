525c842e5f43558351b292c1a12e2dd1e0f16f8e
Dante Culaciati <programmingdante@gmail.com>
2024-04-06 01:20:20 -0300

    Add basic models

--- hackitba_2024/exercises/models.py
+++ hackitba_2024/exercises/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from user_auth.models import *
 
-# Create your models here.
+class Exercise(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        abstract = True
+
+class Achievement(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    progress = models.IntegerField(default=0)
+    limits = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+    
+    @property
+    def is_completed(self):
+        return self.progress == 100
---
+++ hackitba_2024/user_auth/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import UserProfile
+
+class UserRegistrationForm(UserCreationForm):
+    date_of_birth = forms.DateField(label='Date of Birth', required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'date_of_birth']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+            UserProfile.objects.create(user=user, date_of_birth=self.cleaned_data['date_of_birth'])
+        return user
