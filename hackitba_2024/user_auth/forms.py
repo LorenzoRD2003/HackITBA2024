fd4a27472721e73f5c544568579703ca7f1948c0
Lorenzo Ruiz Díaz <lorenzoruizdiaz2003@gmail.com>
2024-04-06 03:32:39 -0300

    Added LoginForm and LoginView templates

--- hackitba_2024/user_auth/forms.py
+++ hackitba_2024/user_auth/forms.py
@@ -1,19 +1,26 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 from .models import UserProfile
 
 class UserRegistrationForm(UserCreationForm):
-    date_of_birth = forms.DateField(label='Date of Birth', required=False, widget=forms.DateInput(attrs={'type': 'date'}))
+  date_of_birth = forms.DateField(label='Date of Birth', required=False, widget=forms.DateInput(attrs={'type': 'date'}))
 
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2', 'date_of_birth']
+  class Meta:
+    model = User
+    fields = ['username', 'password1', 'password2', 'date_of_birth']
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        if commit:
-            user.save()
-            UserProfile.objects.create(user=user, date_of_birth=self.cleaned_data['date_of_birth'])
-        return user
+  def save(self, commit=True):
+    user = super().save(commit=False)
+    if commit:
+      user.save()
+      UserProfile.objects.create(user=user, date_of_birth=self.cleaned_data['date_of_birth'])
+    return user
+
+class LoginForm(AuthenticationForm):
+  username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+  password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+  class Meta:
+    model = User
+    fields = ['username', 'password']
--- hackitba_2024/user_auth/views.py
+++ hackitba_2024/user_auth/views.py
@@ -1,4 +1,35 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 from models import *
+from .forms import UserRegistrationForm, LoginForm
 
 # Create your views here.
+class UserRegistrationView(View):
+  form_class = UserRegistrationForm
+  template_name = "registration.html"
+
+  def get(self, request):
+    form = LoginForm()
+    return render(request, self.template_name, {"form": form})
+
+  def post(self, request):
+    form = UserRegistrationForm(data = request.POST)
+    if form.is_valid():
+      # <process form cleaned data>
+      return HttpResponseRedirect("/success/")
+    return render(request, self.template_name, {"form": form})
+
+class LoginView(View):
+  template_name = 'login.html'
+
+  def get(self, request):
+    form = LoginForm()
+    return render(request, self.template_name, {'form': form})
+
+  def post(self, request):
+    form = LoginForm(data = request.POST)
+    if form.is_valid():
+      # Handle successful login
+      return HttpResponseRedirect('/success/')  # Redirect to success page
+    return render(request, self.template_name, {'form': form})
