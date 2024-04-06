e5ab1aa9af43a53353436dc59681a8ed1e5a9540
Dante Culaciati <programmingdante@gmail.com>
2024-04-06 05:22:40 -0300

    SOLO ARCHIVOS

--- hackitba_2024/hackitba_2024/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', include('user_auth.urls')),
-    path('home/', include('exercises.urls'))
-]
