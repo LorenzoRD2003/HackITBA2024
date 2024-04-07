e10e4fdb9baf20844619ad7cf8c7d653f0a419aa
Dante Culaciati <programmingdante@gmail.com>
2024-04-07 01:01:56 -0300

    fix urls

--- hackitba_2024/exercises/urls.py
+++ hackitba_2024/exercises/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
      path('achievements/', AchievementView.as_view(), name='achievements'),
      path('exercise/', ExerciseView.as_view(), name='exercises'),
      path('exercise/ex1_<int:ex_num>', ExerciseOneView.as_view(), name='ex1'),
-     path('exercise/ex2_<int:ex_num>', ExerciseOneView.as_view(), name='ex2'),
-     path('exercise/ex3_<int:ex_num>', ExerciseOneView.as_view(), name='ex3'),
-     path('exercise/ex4_<int:ex_num>', ExerciseOneView.as_view(), name='ex4'),
+     path('exercise/ex2_<int:ex_num>', ExerciseTwoView.as_view(), name='ex2'),
+     path('exercise/ex3_<int:ex_num>', ExerciseThreeView.as_view(), name='ex3'),
+     path('exercise/ex4_<int:ex_num>', ExerciseFourView.as_view(), name='ex4'),
 ]
