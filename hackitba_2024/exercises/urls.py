b560f796520781ec4e4869e6d5e182a9b5809807
Dante Culaciati <programmingdante@gmail.com>
2024-04-07 00:55:39 -0300

    Vuelvo a agregar modulito IA

--- hackitba_2024/exercises/urls.py
+++ hackitba_2024/exercises/urls.py
@@ -8,10 +8,8 @@ urlpatterns = [
      path('info/', InfoView.as_view(), name='info'),
      path('achievements/', AchievementView.as_view(), name='achievements'),
      path('exercise/', ExerciseView.as_view(), name='exercises'),
-     path('exercise/<str:ex_name>', ExerciseOneView.as_view(), name='ex1'),
-     # path('exercise/ex4_<int:num>', ExerciseFourView.as_view(), name='ex4'),
-     # re_path(r'exercise\/ex1_[0-9]+', ExerciseOneView.as_view(), name='ex1'),
-     # re_path(r'exercise\/ex2_[0-9]+', ExerciseTwoView.as_view(), name='ex2'),
-     # re_path(r'exercise\/ex3_[0-9]+', ExerciseThreeView.as_view(), name='ex3'),
-     
+     path('exercise/ex1_<int:ex_num>', ExerciseOneView.as_view(), name='ex1'),
+     path('exercise/ex2_<int:ex_num>', ExerciseOneView.as_view(), name='ex2'),
+     path('exercise/ex3_<int:ex_num>', ExerciseOneView.as_view(), name='ex3'),
+     path('exercise/ex4_<int:ex_num>', ExerciseOneView.as_view(), name='ex4'),
 ]
